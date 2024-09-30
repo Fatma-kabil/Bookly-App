22724f9ec276e1c068e7013882041b10200a3b5d
Fatma_kabil <fatmakhaled112@gmail.com>
2024-09-30 18:37:41 +0200

    change theme

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -9,7 +9,16 @@ class SplashViewBody extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [Image.asset(AssetsData.logo, height: 55)],
+      children: [
+        Image.asset(AssetsData.logo, height: 55),
+        const SizedBox(
+          height: 10,
+        ),
+        const Text(
+          'Read Free Books',
+          textAlign: TextAlign.center,
+        )
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/conatants.dart';
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
+//import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 void main() {
@@ -12,8 +13,12 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
     return GetMaterialApp(
-      theme: ThemeData(scaffoldBackgroundColor: kPrimaryColor),
+      theme: ThemeData(
+        scaffoldBackgroundColor: kPrimaryColor,
+        brightness: Brightness.dark,
+      ),
       debugShowCheckedModeBanner: false,
       home: const SplashView(),
     );
