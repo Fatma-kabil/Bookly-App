2dfadfb8bc93df2de6909d0a262d333629162666
Fatma_kabil <fatmakhaled112@gmail.com>
2024-10-01 08:58:25 +0200

    adding animation

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,24 +1,82 @@
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  late Animation<Offset> slidingAnimationtwo;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration:
+          const Duration(seconds: 2), // Shorter duration for smoother animation
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, -20), end: const Offset(0, 0))
+            .animate(
+      animationController,
+
+      // Smooth easing for animation
+    );
+
+    slidingAnimationtwo =
+        Tween<Offset>(begin: const Offset(0, 20), end: const Offset(0, 0))
+            .animate(
+      animationController,
+
+      // Smooth easing for animation
+    );
+
+    // Start the animation
+    animationController.forward();
+
+    // This listener is unnecessary; `SlideTransition` already rebuilds on animation updates.
+    // slidingAnimation.addListener(() {
+    // setState(() {});
+    // });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Image.asset(AssetsData.logo, height: 55),
-        const SizedBox(
-          height: 10,
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SlideTransition(
+                position: slidingAnimationtwo,
+                child: Image.asset(AssetsData.logo, height: 55)),
+            const SizedBox(height: 15),
+            SlideTransition(
+              position: slidingAnimation,
+              child: const Text(
+                'Read Free Books',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
         ),
-        const Text(
-          'Read Free Books',
-          textAlign: TextAlign.center,
-        )
-      ],
+      ),
     );
   }
 }
