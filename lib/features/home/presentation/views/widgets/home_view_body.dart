a56437eeda6ac21def1eccb546c36fb82df8fd1f
Fatma_kabil <fatmakhaled112@gmail.com>
2024-10-04 20:22:46 +0200

    best seller list view item part one

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_books_list_view.dart';
@@ -20,6 +21,38 @@ class HomeViewBody extends StatelessWidget {
             height: 50,
           ),
           Text('Best Seller', style: Styles.titleMeduim),
+          SizedBox(
+            height: 18,
+          ),
+          BestSellerListViewItem()
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 140,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.7 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.red,
+                  image: const DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage(
+                        AssetsData.testImage,
+                      ))),
+            ),
+          )
         ],
       ),
     );
