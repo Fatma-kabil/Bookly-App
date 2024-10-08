12a5948fc04bfd4f8064c2603cd9083e3869fea9
Fatma_kabil <fatmakhaled112@gmail.com>
2024-10-08 08:47:01 +0200

    best seller list view done

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,8 @@
 import 'package:bookly_app/conatants.dart';
 
 import 'package:bookly_app/core/utils/styles.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view.dart';
+
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_books_list_view.dart';
 
@@ -12,24 +13,38 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 24, top: 24, bottom: 24),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const CustomAppBar(),
-          const FeaturedBooksListView(),
-          const SizedBox(
-            height: 50,
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 30),
+              child: CustomAppBar(),
+            ),
+            const FeaturedBooksListView(),
+            const SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Text('Best Seller',
+                  style:
+                      Styles.textStyle21.copyWith(fontFamily: kGtSectraFine)),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+          ],
+        )),
+        const SliverFillRemaining(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: BestSellerListView(),
           ),
-          Text('Best Seller',
-              style: Styles.textStyle21.copyWith(fontFamily: kGtSectraFine)),
-          const SizedBox(
-            height: 15,
-          ),
-          const BestSellerListViewItem()
-        ],
-      ),
+        )
+      ],
     );
   }
 }
