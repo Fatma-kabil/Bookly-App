af95352d7c0e2eebea04035f3e8ace1c69b68436
Fatma_kabil <fatmakhaled112@gmail.com>
2024-10-31 10:07:33 +0200

    all done ( add flutter_native_splash)

--- lib/conatants.dart
+++ lib/conatants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xff100B20);
-const kTransitionAnimation = Duration(microseconds: 250);
+const kPrimaryColor =  Color(0xff100B20) ;
 const kGtSectraFine = 'GT Sectra Fine';
+const kTransitionAnimation = Duration(microseconds: 250);
  Color? kshimmercolor = Colors.grey[600];
