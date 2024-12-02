86af053c386f4d380aef3dc7a785423523d70b5b
Fatma_kabil <fatmakhaled112@gmail.com>
2024-12-02 10:28:06 +0200

    create home repo

---
+++ lib/features/home/domain/repos/home_repo.dart
@@ -0,0 +1,10 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/features/home/domain/entites/book_entity.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookEntity>>> fetctNewestBooks();
+  Future<Either<Failure, List<BookEntity>>> fetctFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetctSimilerBooks(
+      {required String category});
+}
