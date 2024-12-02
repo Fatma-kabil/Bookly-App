efbd191be281161c1c7dbddd749ab6355dfbd82e
Fatma_kabil <fatmakhaled112@gmail.com>
2024-12-02 14:04:15 +0200

    fetch similar books use case

--- lib/features/home/domain/use_case/fetch_similar_books_use_case.dart
+++ lib/features/home/domain/use_case/fetch_similar_books_use_case.dart
@@ -5,13 +5,13 @@ import 'package:bookly_app/features/home/domain/entites/book_entity.dart';
 import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchNewestBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
+class FetchSimilarBooksUseCase extends UseCase<List<BookEntity>, String> {
   final HomeRepo homeRepo;
 
-  FetchNewestBooksUseCase(this.homeRepo);
+  FetchSimilarBooksUseCase(this.homeRepo);
 
   @override
-  Future<Either<Failure, List<BookEntity>>> call(NoParam param) async {
-    return await homeRepo.fetctNewestBooks();
+  Future<Either<Failure, List<BookEntity>>> call(String category)async {
+    return await homeRepo.fetctSimilerBooks(category: category);
   }
 }
