aa5550ec86b16d8c536decb9dc0f4f65be5d64e4
Fatma_kabil <fatmakhaled112@gmail.com>
2024-12-04 12:50:42 +0200

    implement remote fetch featured books

--- lib/features/home/data/data_sources/home_reomte_data_source.dart
+++ lib/features/home/data/data_sources/home_reomte_data_source.dart
@@ -1,3 +1,5 @@
+import 'package:bookly_app/core/models/book_model/book_model.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/features/home/domain/entites/book_entity.dart';
 
 abstract class HomeReomteDataSource {
@@ -7,10 +9,25 @@ abstract class HomeReomteDataSource {
 }
 
 class HomeReomteDataSourceImpl extends HomeReomteDataSource {
+  final ApiService apiService;
+
+  HomeReomteDataSourceImpl({required this.apiService});
+
   @override
-  Future<List<BookEntity>> fetctFeaturedBooks() {
-    // TODO: implement fetctFeaturedBooks
-    throw UnimplementedError();
+  Future<List<BookEntity>> fetctFeaturedBooks() async {
+    var data = await apiService.get(
+        endPoint: 'volumes?q=Programming&Filtering=free-ebooks');
+
+    List<BookEntity> books = getBooksList(data);
+    return books;
+  }
+
+  List<BookEntity> getBooksList(Map<String, dynamic> data) {
+     List<BookEntity> books = [];
+    for (var item in data['items']) {
+      books.add(BookModel.fromJson(item));
+    }
+    return books;
   }
 
   @override
